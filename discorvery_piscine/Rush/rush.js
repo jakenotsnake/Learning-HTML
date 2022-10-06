7b5cbe231de2a8418cdc4415ee174b313328a1ae
jakenotsnake <106646682+jakenotsnake@users.noreply.github.com>
2022-10-06 16:58:16 +1030

    website design done

--- discorvery_piscine/Rush/rush.js
+++ discorvery_piscine/Rush/rush.js
@@ -29,18 +29,27 @@ function navdown() {
         setTimeout(() => {
             scrollpage(p);
         }, 630);
+        setglow(cur);
     }
 
     function scrollup(){
-        if (cur == 0)
-            return;
-        cur--;
+        if (cur != 0){
+        cur--;}
         const pages = ['home', 'me', 'hist', 'edu'];
         var p = document.getElementById(pages[cur]);
+        setglow(cur);
+        if (cur <= 0){
+            var p = document.getElementById(pages[0]);
         scrollpage(p);
         setTimeout(() => {
             navdown();
         }, 100);
+        return;}
+        scrollpage(p);
+        setTimeout(() => {
+            navdown();
+        }, 100);
+        
     }
 
     function scrollpage(p){
@@ -50,4 +59,51 @@ function navdown() {
         });
     }
 
-``
+    function scrolto(i){
+    const pages = ['home', 'me', 'hist', 'edu'];
+    var p = document.getElementById(pages[i]);
+    setglow(i);
+    if (i > cur){
+    navdown();
+        setTimeout(() => {
+            scrollpage(p);
+        }, 630);}     
+    else {
+        scrollpage(p);
+        setTimeout(() => {
+            navdown();
+        }, 100);
+    }
+    cur = i;
+}
+
+    function setglow(i){
+        var h = document.getElementById("11");
+        var a = document.getElementById("22");
+        var e = document.getElementById("33");
+        var ed = document.getElementById("44")
+        if (i == 0){
+            h.style.fontSize = "1.4vw";
+        }
+        else {
+            h.style.fontSize = "0.9vw";
+        }
+        if (i == 1){
+            a.style.fontSize = "1.4vw";
+        }
+        else {
+            a.style.fontSize = "0.9vw";
+        }
+        if (i == 2){
+            e.style.fontSize = "1.4vw";
+        }
+        else {
+            e.style.fontSize = "0.9vw";
+        }
+        if (i == 3){
+            ed.style.fontSize = "1.4vw";
+        }
+        else {
+            ed.style.fontSize = "0.9vw";
+        }
+    }
